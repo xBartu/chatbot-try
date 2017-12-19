e7a7706c66926f7c2916807743d1e437d48529f2
Bartu Demirkıran <demirkiran.bartud@gmail.com>
2017-12-19 04:43:22 +0200

    fixs

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+from bot import *
 if __name__ == '__main__':
     from sys import argv
-    print argv[1]
+    bot = Bot()
+    if argv[1] == "train":
+        bot.train()
+    elif argv[1] == "game":
+        bot.give_answer()
+    else:
+        print "something bad happened"
